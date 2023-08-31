17390f66c07f371805084caf2ce892b921119886
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-08-31 17:35:10 +0200

    feat: button e collegamento Js

--- js/main.js
+++ js/main.js
@@ -35,18 +35,19 @@ createApp({
         show: false,
         lineThrough: "",
       },
+
+      buttonAdd: ''
     };
   },
 
+  // MILESTONE 2
+  // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
   methods: {
     deleteTask(index) {
-      this.todoList.splice(index, 1)
+      this.todoList.splice(index, 1);
     },
   },
 }).mount("#app");
-
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
